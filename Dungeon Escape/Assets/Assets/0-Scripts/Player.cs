aa6129e30abb3ab282d04727c959128d6ddc7a9a
Dennis Podkolzin <podkolzin@live.ca>
2022-02-10 12:05:08 -0500

    Movement rework

--- Dungeon Escape/Assets/Assets/0-Scripts/Player.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Player.cs
@@ -6,35 +6,12 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rigidBody = default;
     [SerializeField] private float jumpForce = 5;
-    [SerializeField] private bool grounded = false;
 
-    private bool resetJumpNeeded = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool resetJump = false;
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         Movement();
-        CheckGrounded();
-    }
-
-    private void CheckGrounded()
-    {
-        //Raycast layer mask is a 32bit interger array
-        //To modify it, a bit has to be present on index 8
-        //This is why there is a bit shift to place a 1 at index 8
-        RaycastHit2D groundHit = Physics2D.Raycast(transform.position, Vector2.down, 0.7f, 1 << 8);
-        Debug.DrawRay(transform.position, -Vector2.up * 0.7f, Color.red);
-
-        if (groundHit.collider != null)
-        {
-            Debug.Log($"Hit {groundHit.collider.name}");
-            if (!resetJumpNeeded) { grounded = true; }
-        }
     }
 
     private void Movement()
@@ -42,20 +19,35 @@ public class Player : MonoBehaviour
         float move = Input.GetAxisRaw("Horizontal");
         rigidBody.velocity = new Vector2(move, rigidBody.velocity.y);
 
-
-        if (Input.GetButtonDown("Jump") && grounded)
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
-            grounded = false;
-            resetJumpNeeded = true;
-            StartCoroutine(ResetJumpNeededRoutine());
+            StartCoroutine(ResetJumpRoutine());
         }
-
     }
 
-    IEnumerator ResetJumpNeededRoutine()
+    private bool IsGrounded()
     {
+        //Raycast layer mask is a 32bit interger array
+        //To modify it, a bit has to be present on index 8
+        //This is why there is a bit shift to place a 1 at index 8
+        RaycastHit2D groundHit = Physics2D.Raycast(transform.position, Vector2.down, 0.7f, 1 << 8);
+        if(resetJump) { return false; }
+
+        if(groundHit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }    
+
+    private IEnumerator ResetJumpRoutine()
+    {
+        resetJump = true;
         yield return new WaitForSeconds(0.1f);
-        resetJumpNeeded = false;
+        resetJump = false;
     }
 }
