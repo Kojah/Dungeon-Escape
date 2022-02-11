5b78ecb21b8ff75ac5884f28b2436a639ecd81a7
Dennis Podkolzin <podkolzin@live.ca>
2022-02-11 10:36:59 -0500

    Update Enemy.cs

--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Enemy.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Enemy.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour
 {
-    protected int health;
-    protected int speed;
-    protected int gems;
+    [SerializeField] protected int health;
+    [SerializeField] protected int speed;
+    [SerializeField] protected int gems;
 
     public virtual void Attack()
     {
