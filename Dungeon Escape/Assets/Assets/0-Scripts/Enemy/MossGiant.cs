caec69f8769beb194ddad11af0d6f8b82fadce57
Dennis Podkolzin <podkolzin@live.ca>
2022-02-11 15:21:19 -0500

    Movement and spider

--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/MossGiant.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/MossGiant.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MossGiant : Enemy
 {
     [SerializeField] private Vector3 currentTarget = default;
+    [SerializeField] private Animator animator = default;
+    [SerializeField] private SpriteRenderer sprite = default;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +16,36 @@ public class MossGiant : Enemy
     // Update is called once per frame
     public override void Update()
     {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        
         Movement();
     }
 
     private void Movement()
     {
+        if (currentTarget == pointA.position)
+        {
+            sprite.flipX = true;
+        }
+        else if (currentTarget == pointB.position)
+        {
+            sprite.flipX = false;
+        }
+
         if (transform.position == pointA.position)
         {
             currentTarget = pointB.position;
+            animator.SetTrigger("Idle");
+
         }
         else if (transform.position == pointB.position)
         {
             currentTarget = pointA.position;
+            animator.SetTrigger("Idle");
+
         }
 
         transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
