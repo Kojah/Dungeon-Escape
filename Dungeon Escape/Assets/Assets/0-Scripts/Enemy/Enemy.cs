96b3443c75fc833c02bd542a803f0ad6a42c8b1c
Dennis Podkolzin <podkolzin@live.ca>
2022-02-11 16:21:24 -0500

    Inheritance!
    I knew this already but following course so

--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Enemy.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Enemy.cs
@@ -9,10 +9,51 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] protected int gems;
     [SerializeField] protected Transform pointA, pointB;
 
+    [SerializeField] protected Vector3 currentTarget = default;
+    [SerializeField] protected Animator animator = default;
+    [SerializeField] protected SpriteRenderer sprite = default;
+
+    public virtual void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        Movement();
+    }
+
     public virtual void Attack()
     {
 
     }
 
-    public abstract void Update();
+    public virtual void Movement()
+    {
+        if (currentTarget == pointA.position)
+        {
+            sprite.flipX = true;
+        }
+        else if (currentTarget == pointB.position)
+        {
+            sprite.flipX = false;
+        }
+
+        if (transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            animator.SetTrigger("Idle");
+
+        }
+        else if (transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            animator.SetTrigger("Idle");
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
+
+    
 }
--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Spider.cs
@@ -4,50 +4,5 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
-    [SerializeField] private Vector3 currentTarget = default;
-    [SerializeField] private Animator animator = default;
-    [SerializeField] private SpriteRenderer sprite = default;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    public override void Update()
-    {
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-        {
-            return;
-        }
-
-        Movement();
-    }
-
-    private void Movement()
-    {
-        if (currentTarget == pointA.position)
-        {
-            sprite.flipX = true;
-        }
-        else if (currentTarget == pointB.position)
-        {
-            sprite.flipX = false;
-        }
-
-        if (transform.position == pointA.position)
-        {
-            currentTarget = pointB.position;
-            animator.SetTrigger("Idle");
-
-        }
-        else if (transform.position == pointB.position)
-        {
-            currentTarget = pointA.position;
-            animator.SetTrigger("Idle");
-
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
-    }
 }
