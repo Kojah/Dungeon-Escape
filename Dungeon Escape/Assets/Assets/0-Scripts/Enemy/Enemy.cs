62ecb6f47df331f677be6077ee3e0c616550b366
Dennis Podkolzin <podkolzin@live.ca>
2022-02-11 10:19:10 -0500

    Initial enemy abstract stuff

--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Enemy.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Enemy.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int health;
+    public int speed;
+    protected int gems;
 
-    // Update is called once per frame
-    void Update()
+    public void Attack()
     {
-        
+
     }
 }
