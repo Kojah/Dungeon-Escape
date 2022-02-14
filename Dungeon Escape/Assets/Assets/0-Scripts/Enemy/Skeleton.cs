efd417e3033b110195315dc474f2677260fadf71
Dennis Podkolzin <podkolzin@live.ca>
2022-02-14 11:49:23 -0500

    Skeleton hit animation setup

--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Skeleton.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Skeleton.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Skeleton : Enemy, IDamageable
 {
+    void Start()
+    {
+        Init();
+    }
+
     public int Health { get; set; }
     public override void Init()
     {
         base.Init();
         Health = health;
+        Debug.Log(health);
     }
 
     public void Damage(int damageTaken)
     {
-        //subtract damage from health
-        //if no health, destroy object
-
+        animator.SetTrigger("Hit");
         Health -= damageTaken;
         if(Health < 0)
         {
--- Dungeon Escape/Assets/Assets/0-Scripts/Player/Attack.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Player/Attack.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    bool coolingDown = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
         IDamageable hit = other.GetComponent<IDamageable>();
-        if(hit != null)
+        if (!coolingDown && hit != null)
         {
             hit.Damage(1);
+            Debug.Log("hit!");
+            coolingDown = true;
+            StartCoroutine(attackCooldown());
         }
+    }
+
+    IEnumerator attackCooldown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        coolingDown = false;
     }
 }
