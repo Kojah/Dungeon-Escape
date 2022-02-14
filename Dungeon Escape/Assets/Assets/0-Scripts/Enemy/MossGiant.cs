95ace83ce5e8e095c96af8652e876f9f34effe6c
Dennis Podkolzin <podkolzin@live.ca>
2022-02-14 16:07:52 -0500

    Update MossGiant.cs

--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/MossGiant.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/MossGiant.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MossGiant : Enemy
+public class MossGiant : Enemy, IDamageable
 {
     public int Health { get; set; }
 
