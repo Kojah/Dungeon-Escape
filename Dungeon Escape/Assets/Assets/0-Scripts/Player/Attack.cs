e7b0dc508449f6b1ac1e2817fe9302964754e743
Dennis Podkolzin <podkolzin@live.ca>
2022-02-14 10:57:00 -0500

    Sword collision and prefab updates

---
+++ Dungeon Escape/Assets/Assets/0-Scripts/Player/Attack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+    }
+}
