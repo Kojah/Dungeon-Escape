773745a1280af3dfdd940f831ae3d5f971267159
Dennis Podkolzin <podkolzin@live.ca>
2022-02-14 11:25:04 -0500

    IDamageable base implementation

--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Skeleton.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Skeleton.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : Enemy
+public class Skeleton : Enemy, IDamageable
 {
+    public int Health { get; set; }
     public override void Init()
     {
         base.Init();
+    }
+
+    public void Damage()
+    {
+
     }
 }
--- Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Assets/0-Scripts/Enemy/Spider.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spider : Enemy
+public class Spider : Enemy, IDamageable
 {
+    public int Health { get; set; }
     public override void Init()
     {
         base.Init();
+    }
+
+    public void Damage()
+    {
+
     }
 }
