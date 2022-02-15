cd2e464b8051f21a6a324f291d69d1b5d86cfca8
Dennis Podkolzin <podkolzin@live.ca>
2022-02-15 11:34:19 -0500

    Shop switching on/off

---
+++ Dungeon Escape/Assets/Assets/0-Scripts/Shop/Shop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPanel = default;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            shopPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
+    }
+}
